a62de902e88e3f6fe5b48f51c3fac29a87637b70
PedramNouri <pnouri@solution-tek.com>
2024-03-10 20:05:17 -0400

    modify gitignore - add student

---
+++ ignore/Student.cs
@@ -0,0 +1,13 @@
+namespace ConsoleApp1.ignore;
+
+public class Student
+{
+    public string id { get; set; }
+    public int age { get; set; }
+
+    public Student(string id, int age)
+    {
+        this.id = id;
+        this.age = age;
+    }
+}
