141e56fd864a9f2b69eece95bbe488200595e744
PedramNouri <pnouri@solution-tek.com>
2024-03-10 20:02:13 -0400

    modify gitignore

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1.ignore;
+
 Console.WriteLine("Hello, Worlddd!");
+Student student = new Student("9431025", 28);
+
+Console.WriteLine(student.age);
